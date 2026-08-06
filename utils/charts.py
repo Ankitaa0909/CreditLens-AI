1272e3bb5af6f9bf490dba934d14520d11ca36e0
Ankita K <144467349+Ankitaa0909@users.noreply.github.com>
2026-08-06 23:49:29 +0530

    Update charts.py

--- utils/charts.py
+++ utils/charts.py
@@ -1,4 +1,6 @@
 import plotly.express as px
+import plotly.graph_objects as go
+
 
 def revenue_chart(df):
 
@@ -10,9 +12,73 @@ def revenue_chart(df):
         title="Monthly Revenue"
     )
 
-    fig.update_layout(
-        template="plotly_dark",
-        height=450
+    return fig
+
+
+def cashflow_chart(df):
+
+    fig = px.bar(
+        df,
+        x="Month",
+        y="Balance",
+        title="Cashflow Trend"
     )
+
+    return fig
+
+
+def upi_chart(df):
+
+    fig = px.line(
+        df,
+        x="Month",
+        y="Amount",
+        markers=True,
+        title="UPI Collections"
+    )
+
+    return fig
+
+
+def employee_chart(df):
+
+    fig = px.bar(
+        df,
+        x="Month",
+        y="Employees",
+        title="Employee Growth"
+    )
+
+    return fig
+
+
+def financial_gauge(score):
+
+    fig = go.Figure(go.Indicator(
+
+        mode="gauge+number",
+
+        value=score,
+
+        title={"text": "Financial Health Score"},
+
+        gauge={
+            "axis": {"range": [0, 100]},
+
+            "bar": {"color": "green"},
+
+            "steps": [
+
+                {"range": [0, 40], "color": "#ff4d4d"},
+
+                {"range": [40, 70], "color": "#ffd633"},
+
+                {"range": [70, 100], "color": "#66cc66"}
+
+            ]
+
+        }
+
+    ))
 
     return fig
