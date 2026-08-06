9ad6a6bfce0a88a30fd249e20c5e089a426faf86
Ankita K <144467349+Ankitaa0909@users.noreply.github.com>
2026-08-06 23:51:13 +0530

    Update cashflow_analysis.py

--- models/cashflow_analysis.py
+++ models/cashflow_analysis.py
@@ -1,7 +1,26 @@
-Average balance
+class CashflowAnalysis:
 
-Monthly income
+    def __init__(self, bank_df):
+        self.df = bank_df
 
-Monthly expense
+    def average_balance(self):
+        return round(self.df["Balance"].mean(), 2)
 
-Net cashflow
+    def minimum_balance(self):
+        return round(self.df["Balance"].min(), 2)
+
+    def maximum_balance(self):
+        return round(self.df["Balance"].max(), 2)
+
+    def cashflow_trend(self):
+
+        first = self.df["Balance"].iloc[0]
+        last = self.df["Balance"].iloc[-1]
+
+        if last > first:
+            return "Increasing"
+
+        elif last < first:
+            return "Decreasing"
+
+        return "Stable"
