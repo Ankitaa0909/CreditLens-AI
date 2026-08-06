80d9c50ea25cfca5adf6c5bd71d8452c381bd187
Ankita K <144467349+Ankitaa0909@users.noreply.github.com>
2026-08-06 23:50:50 +0530

    Update loan_recommendation.py

--- models/loan_recommendation.py
+++ models/loan_recommendation.py
@@ -1,8 +1,60 @@
-if score > 80:
-    Working Capital Loan
+class LoanRecommendation:
 
-elif score > 65:
-    Equipment Loan
+    @staticmethod
+    def recommend(score):
 
-else:
-    Micro Business Loan
+        if score >= 85:
+
+            return {
+
+                "loan": "Working Capital Loan",
+
+                "amount": "₹25,00,000",
+
+                "interest": "9.2%",
+
+                "tenure": "5 Years"
+
+            }
+
+        elif score >= 70:
+
+            return {
+
+                "loan": "Business Expansion Loan",
+
+                "amount": "₹15,00,000",
+
+                "interest": "10.5%",
+
+                "tenure": "4 Years"
+
+            }
+
+        elif score >= 55:
+
+            return {
+
+                "loan": "Micro Enterprise Loan",
+
+                "amount": "₹5,00,000",
+
+                "interest": "11.8%",
+
+                "tenure": "3 Years"
+
+            }
+
+        else:
+
+            return {
+
+                "loan": "Not Eligible",
+
+                "amount": "-",
+
+                "interest": "-",
+
+                "tenure": "-"
+
+            }
